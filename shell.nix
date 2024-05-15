341916ea287c5ba5cdae8bbe1ac508312e61292f
Arsen Nuramatov <anuramat@pm.me>
2024-05-15 19:20:52 +0200

    feat(nix): use native packages

--- shell.nix
+++ shell.nix
@@ -1,22 +1,20 @@
-{pkgs ? import <nixpkgs> {}}: let
-  fhs = pkgs.buildFHSUserEnv {
-    name = "my-fhs-environment";
-
-    targetPkgs = _: [
-      pkgs.micromamba
-    ];
-
-    profile = ''
-      set -e
-      eval "$(micromamba shell hook --shell=posix)"
-      export MAMBA_ROOT_PREFIX=${builtins.getEnv "PWD"}/.mamba
-      if ! test -d $MAMBA_ROOT_PREFIX/envs/my-mamba-environment; then
-          micromamba create --yes -q -n my-mamba-environment
-      fi
-      micromamba activate my-mamba-environment
-      micromamba install --yes -f conda-requirements.txt -c conda-forge
-      set +e
-    '';
-  };
+let
+  pkgs = import <nixos-unstable> {};
 in
-  fhs.env
+  pkgs.mkShell {
+    packages = [
+      (pkgs.python3.withPackages (python-pkgs:
+        with python-pkgs; [
+          numpy
+          matplotlib
+          scikit-image
+          scikit-learn
+          jupyter
+          torch
+          torchvision
+          pytorch-lightning
+          # jupyter-collaboration
+          jupytext
+        ]))
+    ];
+  }
